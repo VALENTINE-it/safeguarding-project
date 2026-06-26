bbe0cefdb9121fc42283ad17dc2ee785c58eaea5
Valentine <valentineawili@gmail.com>
2026-06-27 02:34:18 +0300

    Feat: Added features like the radio selection on the page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function SafeGuardingApp() {
+  return (
+
+    <>
+    <div className="Header">
+     <h1>🛡️ SafeGuarding</h1>
+    </div>
+
+    <div className="Body">
+    <div className="Intro">
+      <h1 className="H1">Secure communication, </h1>
+      <h1 className="H1">Absolut Privacy </h1>
+
+      <p>A minimalist space for high-stakes messaging. No metadata tracking, no data harvesting. Just clarity.</p>
+    </div>
+
+    <div>
+      <div className="card">
+  <h3>Initiate Transmission</h3>
+  <p>Select your messaging path to continue.</p>
+
+  <label className="option">
+    <input type="radio" name="path" value="new" defaultChecked />
+    <div>
+      <div className="option-title">New Message</div>
+      <div className="option-desc">Start a clean, encrypted conversation thread.</div>
+    </div>
+  </label>
+
+  <label className="option">
+    <input type="radio" name="path" value="follow" />
+    <div>
+      <div className="option-title">Follow up</div>
+      <div className="option-desc">Access an existing thread via Secure Token.</div>
+    </div>
+  </label>
+
+  <button>Continue →</button>
+
+  <div className="badge">🔒 Anonymous Messaging</div>
+</div>
+
+    </div>
+    </div>
+
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SafeGuardingApp />
   </React.StrictMode>
 );
 
